c50395ebd2ebdfdc3d268c3f1dca7e9e02becca9
provee510 <providencecharles@yahoo.com>
2024-09-30 06:50:23 -0700

    second commit

--- src/controllers/property/index.js
+++ src/controllers/property/index.js
@@ -1,1 +1,100 @@
-import Property from '../../models/property'
+import Property from "../../models/property.js"
+
+
+// createProperty
+export const createProperty = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err.message);
+        
+        
+    }
+}
+
+// updateProperty
+export const updateProperty = async (req, res) => {
+    try {
+        const { title, description, price, location} = req.body;
+
+     // Handle validation
+
+     // Check for required fields
+        // if (!title ||!description ||!price ||!location ||!password) {
+        //     return res.status(400).json({ msg: 'Please enter all fields' });
+        // }
+        if(!title){
+            return res.status(400).json({ success: false, message: "title name is required." });
+        },
+        if(!description){
+            return res.status(400).json({ success: false, message: "description name is required." });
+        },
+        if(!price){
+            return res.status(400).json({ success: false, message: "price name is required." });
+        },
+        if(!location){
+            return res.status(400).json({ success: false, message: "location name is required." });
+        }
+
+        
+
+    } catch (err) {
+        console.log(err.message);
+        
+        
+    }
+}
+
+// getAllProperties
+export const getAllProperties = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err.message);
+        
+        
+    }
+}
+
+// getPropertyById
+export const getPropertyById = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err.message);
+        
+        
+    }
+}
+
+// filterProperties
+export const filterProperties = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err.message);
+        
+        
+    }
+}
+
+// deleteProperty
+export const deleteProperty = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err.message);
+        
+        
+    }
+}
+
+export const searchProperty = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err.message);
+        
+        
+    }
+}
--- src/models/property.js
+++ src/models/property.js
@@ -1,92 +1,159 @@
-import mongoose from 'mongoose';
-const {schema} = mongoose;
+// import mongoose from 'mongoose';
+// const {schema} = mongoose;
 
-const PropertySchema = new Schema({
+// const PropertySchema = new Schema({
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     bedrooms :{
+//         type: Number,
+//         default, 3
+//     },
+//     bathrooms: {
+//         type: Number,
+//         default, 3
+//     },
+//     postedBy: {
+//         type: ObjectId,
+//         ref: 'User'
+//     },
+//     isActive : {
+//         type: Boolean,
+//         default: true
+//     },
+//     location: {
+//         type: String,
+//         required: true
+//     },
+//     price: {
+//         type: Number,
+//         required: true
+//     },
+//     type: {
+//         type: String,
+//         required: true
+//     },
+//     size: {
+//         type: String,
+//         required: true
+//     },
+//     propertyType: {
+//         type: String,
+//         default: 'appartment',
+//         enum : [
+//             'apartment',
+//             'house',
+//             'condo',
+//             'townhouse',
+//             'villa',
+//             'land',
+//             'commercial',
+//             'other'
+//         ]
+//     },
+
+//     images: {
+//         type: [String],
+//         required: true
+//     },
+    
+//     user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User',
+//         required: true
+//     },
+//     status: {
+//         type: String,
+//         enum: ['active', 'inactive', 'sold'],
+//         default: 'active'
+//     },
+//     views: {
+//         type: Number,
+//         default: 0
+//     },
+//     favorites: {
+//         type: Number,
+//         default: 0
+//     },
+//     images: [{
+//         url: {
+//           type: String,
+//         },
+//         imagePublicId: {
+//           type: String,
+//         }
+//       }], 
+    
+// }, timestamp: true)
+
+
+
+
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const { ObjectId } = Schema;
+
+const PropertySchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
-    },
-    bedrooms :{
-        type: Number,
-        required: true
-    },
-    bathrooms: {
-        type: Number,
-        required: true
-    },
-    postedBy: {
-        type: ObjectId,
-        ref: 'User'
-    },
-    isActive : {
-        type: Boolean,
-        default: true
-    },
-    location: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      unique: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
-    type: {
-        type: String,
-        required: true
+    bedrooms: {
+      type: Number,
+      default: 3,
     },
-    size: {
-        type: String,
-        required: true
+    bathrooms: {
+      type: Number,
+      default: 3,
+        },
+    sqm: {
+      type: Number,
+      default: 3000
+    },
+    location: {
+      type: String,
+      required: true,
     },
     propertyType: {
-        type: String,
-        default: 'appartment',
-        enum : [
-            'apartment',
-            'house',
-            'condo',
-            'townhouse',
-            'villa',
-            'land',
-            'commercial',
-            'other'
-        ]
+      type: String,
+      default: "apartment",
+      enum: ["house", "office", "villa", "land"],
     },
-
-    images: {
-        type: [String],
-        required: true
-    },
-    
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ['active', 'inactive', 'sold'],
-        default: 'active'
-    },
-    views: {
-        type: Number,
-        default: 0
-    },
-    favorites: {
-        type: Number,
-        default: 0
-    },
-    images: [{
+    images: [
+      {
         url: {
           type: String,
         },
         imagePublicId: {
           type: String,
-        }
-      }], 
-    
-}, timestamp: true)
+        },
+      },
+    ],
+    postedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
