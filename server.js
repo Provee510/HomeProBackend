539db912cdceb737637d01200f59650b0906310e
provee510 <providencecharles@yahoo.com>
2024-09-30 06:18:58 -0700

    update

--- server.js
+++ server.js
@@ -1,39 +1,87 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import AuthRouter from './src/routes/auth.js'
-import { globalMiddleWare } from './src/middlewares/auth.js';
-import { authCheck } from './src/middlewares/auth.js';
-import morgan from 'morgan'
-import { connectDB } from './src/config/dbConfig.js';
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import AuthRouter from './src/routes/auth.js'
+// import { globalMiddleWare } from './src/middlewares/auth.js';
+// import { authCheck } from './src/middlewares/auth.js';
+// import morgan from 'morgan'
+// import { connectDB } from './src/config/dbConfig.js';
+// dotenv.config();
+
+// const app = express();
+// app.use(express.json()); // for passing res body
+
+
+// const port = process.env.PORT || 8000;
+// const dbUrl = process.env.MONGODB_URL;
+// connectDB(dbUrl);
+// const data = 'server is running live';
+
+// // middlewares
+// app.use(globalMiddleWare)
+// app.use(morgan('dev'));
+
+
+// app.get('/', (req, res) => {
+//   res.json(data);
+// });
+//  // Authentication middleware
+
+
+// // Auth Router
+// app.use('/api/auth', AuthRouter); //localhost/api/auth/login
+
+
+
+
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import AuthRouter from "./src/routes/auth.js";
+// import { globalMiddleware } from "./src/middlewares/auth.js";
+import { connectDB } from "./src/config/dbConfig.js";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
-app.use(express.json()); // for passing res body
 
+app.use(express.json()); // For parsing req body
+
+// Enable CORS
+app.use(cors())
+
+// middlewares
+app.use(morgan("dev"));
+// app.use(globalMiddleware)
+
+
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "Welcome to Homepro Backend API" });
+});
+// Auth Routes
+app.use("/api/auth", AuthRouter); // localhost:8080/api/auth/login
 
 const port = process.env.PORT || 8000;
 const dbUrl = process.env.MONGODB_URL;
-connectDB(dbUrl);
-const data = 'server is running live';
 
-// middlewares
-app.use(globalMiddleWare)
-app.use(morgan('dev'));
-
-
-app.get('/', (req, res) => {
-  res.json(data);
-});
- // Authentication middleware
-
-
-// Auth Router
-app.use('/api/auth', AuthRouter); //localhost/api/auth/login
-
-
-
-
+// Connect to MongoDB database
+connectDB(dbUrl)
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,144 +1,297 @@
-import User from "../models/user.js"
-import { hashPassword } from "../helpers/index.js";
-import { comparePassword } from "../helpers/index.js";
-import  jwt  from "jsonwebtoken";
+// import User from "../models/user.js"
+// import { hashPassword } from "../helpers/index.js";
+// import { comparePassword } from "../helpers/index.js";
+// import  jwt  from "jsonwebtoken";
 
 
 
-export const register = async (req, res)=>{
-    try {
-        const { firstName, lastName, userName, email, password } = req.body;
-        // console.log(req.body);
+// export const register = async (req, res)=>{
+//     try {
+//         const { firstName, lastName, userName, email, password } = req.body;
+//         // console.log(req.body);
 
-        // Handle validation
-        if(!firstName){
-            return res.status(400).json({success:false,message:"First name is required."});
-        }
-        if(!lastName){
-            return res.status(400).json({success:false,message:"Last name is required."});
-        }
-        if(!userName){
-            return res.status(400).json({success:false,message:"userName is taken."});
-        }
-        if(!email){
-            return res.status(400).json({success:false,message:"Email is required."});
-        }
-        if(! /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
-            return res.status(400).json({success:false,message:"Invalid email format."});
-        }
-        if(!password){
-            return res.status(400).json({success:false,message:"Password is required."});
-        }
-        if(!password || password.length <6){
-            return res.status(400).json({success:false,message:"Password must be at least 6 characters long."});
-        }
+//         // Handle validation
+//         if(!firstName){
+//             return res.status(400).json({success:false,message:"First name is required."});
+//         }
+//         if(!lastName){
+//             return res.status(400).json({success:false,message:"Last name is required."});
+//         }
+//         if(!userName){
+//             return res.status(400).json({success:false,message:"userName is taken."});
+//         }
+//         if(!email){
+//             return res.status(400).json({success:false,message:"Email is required."});
+//         }
+//         if(! /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+//             return res.status(400).json({success:false,message:"Invalid email format."});
+//         }
+//         if(!password){
+//             return res.status(400).json({success:false,message:"Password is required."});
+//         }
+//         if(!password || password.length <6){
+//             return res.status(400).json({success:false,message:"Password must be at least 6 characters long."});
+//         }
 
-        // check if email already exists
-        const userExists = await User.findOne ({email: email});
-        if(userExists){
-            return res.status(400).json({success: false, message: "Email already exists"})
-        }
+//         // check if email already exists
+//         const userExists = await User.findOne ({email: email});
+//         if(userExists){
+//             return res.status(400).json({success: false, message: "Email already exists"})
+//         }
 
 
-        // check if userName is taken 
-        const userNameTaken = await User.findOne ({userName: userName});
-        if(userNameTaken){
-            return res.status(400).json({success: false, message: ` username "${userName}" already taken`})
-        }
+//         // check if userName is taken 
+//         const userNameTaken = await User.findOne ({userName: userName});
+//         if(userNameTaken){
+//             return res.status(400).json({success: false, message: ` username "${userName}" already taken`})
+//         }
         
-        // hashing user password 
-        const hashedPassword =await hashPassword(password);
+//         // hashing user password 
+//         const hashedPassword =await hashPassword(password);
   
-        // create a new user
-        const user =new User({
-            firstName,
-            lastName,
-            userName,
-            email,
-            password:hashedPassword,
-        })
+//         // create a new user
+//         const user =new User({
+//             firstName,
+//             lastName,
+//             userName,
+//             email,
+//             password:hashedPassword,
+//         })
 
-        // save the user to the database 
-        await user.save();
+//         // save the user to the database 
+//         await user.save();
 
-        // Generate a token 
-        const token =jwt.sign ({id:user._id}, process.env.JWT_SECRET,{expiresIn: "2h"});
+//         // Generate a token 
+//         const token =jwt.sign ({id:user._id}, process.env.JWT_SECRET,{expiresIn: "2h"});
 
-        // send response 
-        return res.json({success:true, message:'Registration successful', 
-            user:{
-                firstName:user.firstName,
-                lastName:user.lastName,
-                userName:user.userName,
-                email:user.email,
-                role:user.role,
-                token
+//         // send response 
+//         return res.json({success:true, message:'Registration successful', 
+//             user:{
+//                 firstName:user.firstName,
+//                 lastName:user.lastName,
+//                 userName:user.userName,
+//                 email:user.email,
+//                 role:user.role,
+//                 token
                 
-            }});
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({success:false, message: "Registration failed", error: err.message});
-    }
+//             }});
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).json({success:false, message: "Registration failed", error: err.message});
+//     }
     
-}
+// }
+
+
+
+// export const login = async (req, res) => {
+//     try { 
+//         const { userName, password } = req.body;
+
+//         // Check if username is provided
+//         if (!userName) {
+//             return res.status(404).json({ success: false, message: 'Username is required' });
+//         }
+
+//         // Check if password is provided
+//         if (!password) {
+//             return res.status(404).json({ success: false, message: 'Password is required' });
+//         }
+
+//         // Find user by username
+//         const user = await User.findOne({ userName });
+//         if (!user) {
+//             return res.status(404).json({ success: false, message: 'User not found' });
+//         }
+
+//         console.log(user);
+        
+
+//         // Now, use your imported comparePassword function to verify the password
+//         const isMatch = await comparePassword(password, user.password);
+//         if (!isMatch) {
+//             return res.status(400).json({ success: false, message: 'Incorrect password' });
+//         }
+
+//         // Generate a token
+//         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "2h" });
+
+//         // Send response with user details and token
+//         return res.json({
+//             success: true,
+//             message: "Login successful",
+//             user: {
+//                 firstName: user.firstName,
+//                 lastName: user.lastName,
+//                 username: user.userName,
+//                 email: user.email,
+//                 role: user.role,
+//                 token
+//             }
+//         });
+
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(500).json({ success: false, message: 'Login failed', error: err.message });
+//     }
+// };
+
+
+// // forgetpassword
+
+// // resetpassword
+
+// // socialAuth e.g Google
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import User from "../models/user.js";
+import { hashPassword, comparePassword } from "../helpers/index.js";
+import jwt from "jsonwebtoken"; 
+
+export const register = async (req, res) => {
+  try {
+    const { firstName, lastName, userName, email, password } = req.body;
+    // console.log(req.body);
+
+    // Handle validation 
+    if(!firstName){
+        return res.status(400).json({ success: false, message: "First name is required." });
+    }
+    if(!lastName){
+        return res.status(400).json({ success: false, message: "Last name is required." });
+    }
+    if(!userName){
+        return res.status(400).json({ success: false, message: "User name is required." });
+    }
+    if(!email){
+        return res.status(400).json({ success: false, message: "Email is required." });
+    }
+    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+        return res.status(400).json({ success: false, message: "Invalid email format." });
+    }
+    if(!password){
+        return res.status(400).json({ success: false, message: "Password is required." });
+    }
+    if(password.length < 6){
+        return res.status(400).json({ success: false, message: "Password must be at least 6 characters long." });
+    }
+
+    // Check if email already exists
+    const userExists = await User.findOne({ email: email});
+    if(userExists){
+        return res.status(400).json({ success: false, message: "Email already exists"})
+    }
+
+    // Check if userName is taken
+    const userNameTaken = await User.findOne({ userName: userName });
+    if(userNameTaken){
+        return res.status(400).json({ success: false, message: `Username "${userName}" already taken`})
+    }
+
+    // hashing user password
+    const hashedPassword = await hashPassword(password);
+
+    // Create a new user
+    const user = new User({
+        firstName,
+        lastName,
+        userName,
+        email,
+        password: hashedPassword,
+    })
+
+    // Save the user to the database
+    await user.save();
+
+    // Generate a token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    // Send response
+    return res.json({ success: true, message: "Registration successful", user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.userName,
+        email: user.email,
+        role: user.role,
+        token
+    }});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "Registration failed", error: err });
+  }
+};
+
+
 
 
 
 export const login = async (req, res) => {
-    try { 
-        const { userName, password } = req.body;
+  try { 
+      const { userName, password } = req.body;
 
-        // Check if username is provided
-        if (!userName) {
-            return res.status(404).json({ success: false, message: 'Username is required' });
-        }
+      // Check if username is provided
+      if (!userName) {
+          return res.status(404).json({ success: false, message: 'Username is required' });
+      }
 
-        // Check if password is provided
-        if (!password) {
-            return res.status(404).json({ success: false, message: 'Password is required' });
-        }
+      // Check if password is provided
+      if (!password) {
+          return res.status(404).json({ success: false, message: 'Password is required' });
+      }
 
-        // Find user by username
-        const user = await User.findOne({ userName });
-        if (!user) {
-            return res.status(404).json({ success: false, message: 'User not found' });
-        }
+      // Find user by username
+      const user = await User.findOne({ userName });
+      if (!user) {
+          return res.status(404).json({ success: false, message: 'User not found' });
+      }
 
-        console.log(user);
-        
+      console.log(user);
+      
+      const isMatch = await comparePassword(password, user.password);
+      if (!isMatch) {
+          return res.status(400).json({ success: false, message: 'Incorrect password' });
+      }
 
-        // Now, use your imported comparePassword function to verify the password
-        const isMatch = await comparePassword(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ success: false, message: 'Incorrect password' });
-        }
+      // Generate a token
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "2h" });
 
-        // Generate a token
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "2h" });
+      // Send response with user details and token
+      return res.json({
+          success: true,
+          message: "Login successful",
+          user: {
+              firstName: user.firstName,
+              lastName: user.lastName,
+              username: user.userName,
+              email: user.email,
+              role: user.role,
+              token
+          }
+      });
 
-        // Send response with user details and token
-        return res.json({
-            success: true,
-            message: "Login successful",
-            user: {
-                firstName: user.firstName,
-                lastName: user.lastName,
-                username: user.userName,
-                email: user.email,
-                role: user.role,
-                token
-            }
-        });
-
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ success: false, message: 'Login failed', error: err.message });
-    }
+  } catch (err) {
+      console.error(err);
+      return res.status(500).json({ success: false, message: 'Login failed', error: err.message });
+  }
 };
 
 
-// forgetpassword
+// forgotPassword
 
-// resetpassword
+//  resetPassword
 
-// socialAuth e.g Google
+// socialAuth e. g GoogleAuth
